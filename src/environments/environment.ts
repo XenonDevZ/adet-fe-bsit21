21095739e47c393c755025f0e6a62bc592d8d69d
XenonDevZ <91532450+XenonDevZ@users.noreply.github.com>
2026-04-04 00:37:18 +0800

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://localhost:3000',
+    apiUrl: 'https://acbs-backend.onrender.com',
     googleClientId: '902640356522-8ilob0jeu8njg6khm95uqd4k5m38ifim.apps.googleusercontent.com',
   }
